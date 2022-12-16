6e69d7504796fdde6f9d43cba61351d3614112fa
Ksab070 <khansubhan070@gmail.com>
2022-12-16 18:45:10 +0530

    Simple UI done

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # RockPaperScissors
 Rock-Paper-Scissors project from the ODIN PROJECT!
+1. Logic written - 15/12/2022.
+2. Added a simple UI - 16/12/2022.
--- script.js
+++ script.js
@@ -1,44 +1,78 @@
-let score = 0;
+let playerDisplay = document.querySelector('#playerScore');
+let compDisplay = document.querySelector('#compScore');
+let playerScore = document.querySelector('#playerScore').textContent;
+let compScore = document.querySelector('#compScore').textContent;
+let roundInfo = document.querySelector('#rinfo');
+let gameInfo = document.querySelector('#ginfo');
+let resetBtn = document.querySelector('#reset');
+let n = 0;
 
 function getComputerChoice () {
    let choice = Math.floor(Math.random()*3)+1
    if (choice === 1) {
-    return 'ROCK'
+    return 'rock'
    } else if (choice === 2) {
-    return 'PAPER'
+    return 'paper'
    } else {
-    return 'SCISSORS'
+    return 'scissors'
    }
 }
 
-const computerSelection = getComputerChoice(); 
-
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toUpperCase();
-    if (playerSelection === 'ROCK' && computerSelection === 'PAPER') {
-        score--;
-        console.log('button clicked')
-    } else if (playerSelection === 'PAPER' && computerSelection === 'SCISSORS') {
-        score--;
-        console.log('button clicked')
-    } else if (playerSelection === 'SCISSORS' && computerSelection === 'ROCK') {
-        score--;
-        console.log('button clicked')
-    } else if (playerSelection == computerSelection){
-        score = score;
-        console.log('button clicked')
+    n++;
+    if (playerSelection === computerSelection){
+        if (n>4) {
+            roundInfo.textContent = 'Game Over!';
+            gameOver(playerScore,compScore);
+        } else {
+            roundInfo.textContent = 'Tie';
+        }
+    } 
+    else if (playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'scissors' || playerSelection === 'scissors' && computerSelection === 'rock') 
+    {   if (n>4) {
+            roundInfo.textContent = 'Game Over!';
+            gameOver(playerScore,compScore);
+        } else {
+            compScore++;
+            compDisplay.textContent = compScore;
+            roundInfo.textContent = 'Computer Score +1'
+        }
     } else {
-        score++;
-        console.log('button clicked')
+        if (n>4) {
+            roundInfo.textContent = 'Game Over!';
+            gameOver(playerScore,compScore);
+        } else {
+            playerScore++;
+            playerDisplay.textContent = playerScore;
+            roundInfo.textContent = 'Player Score +1'
+        }
     }
-
   }
 
-let buttons = document.querySelectorAll('button');
-
-for (button of buttons) {
-    button.addEventListener('click', () => {
-        playRound(button.id,computerSelection)
+let buttons = document.querySelectorAll('.btns');
+for (let i =0; i<buttons.length;i++) {
+    buttons[i].addEventListener('click', () => {
+        let id = buttons[i].id;
+        playRound(id,getComputerChoice())
     })
 }
 
+resetBtn.addEventListener('click', ()=> {
+    n = 0;
+    compDisplay.textContent = 0;
+    playerDisplay.textContent = 0;
+    playerScore = 0;
+    compScore = 0;
+    roundInfo.textContent = 'Round Info';
+    gameInfo.textContent = 'Rock,Paper or Scissors?'
+})
+
+function gameOver(playerScore,compScore) {
+    if (playerScore == compScore) {
+        gameInfo.textContent = `TIE! Your score is ${playerScore}`
+    } else if (compScore>playerScore) {
+        gameInfo.textContent = `You Lose! Your score is ${playerScore}`
+    } else {
+        gameInfo.textContent = `You win! Your score is ${playerScore}`
+    }
+}
